11eddc6e4556377540fa6c82209a9c15232d0790
mdfauser <fauser.negotium@gmail.com>
2026-03-19 15:23:20 +0100

    feat: basic structure of PrioritizedReplayBuffer

--- storage/buffer.py
+++ storage/buffer.py
@@ -1,7 +1,46 @@
-class PrioritizedReplayBuffer():
-    def __init__(self):
-        # needs to store (Observation, Action, Reward, Search Policy, Search Value)
-        pass
+import chex
+import jax.numpy as jnp
+import jax
 
-    def store():
-        pass
+
+@chex.dataclass
+class MuZeroTransition:
+    observation: chex.Array
+    action: chex.Array
+    reward: chex.Array
+    root_value: chex.Array
+    child_visits: chex.Array
+
+# needs to store (Observation, Action, Reward, Search Policy, Search Value)
+
+
+@chex.dataclass
+class PrioritizedReplayBuffer:
+    data: MuZeroTransition
+    prioriies: chex.Array
+    position: int
+
+    def add_trajectory(self, new_traj, slot):
+        # Update the nested Transition data
+        new_data = jax.tree_util.tree_map(
+            lambda buf_arr, traj_arr: buf_arr.at[slot].set(traj_arr),
+            self.data,
+            new_traj
+        )
+        # Return a new buffer object with the updated data
+        return self.replace(data=new_data)
+
+
+def init_prioritized_buffer(max_seq, seq_len, obs_shape):
+    buffer_data = MuZeroTransition(
+        observation=jnp.zeros((max_seq, seq_len, *obs_shape)),
+        action=jnp.zeros(
+            (max_seq, seq_len), dtype=jnp.int32),
+        reward=jnp.zeros((max_seq, seq_len)),
+        value=jnp.zeros((max_seq, seq_len))
+    )
+    return PrioritizedReplayBuffer(
+        data=buffer_data,
+        prioriies=jnp.zeros((max_seq,)),
+        position=0
+    )
