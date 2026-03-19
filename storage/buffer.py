85e6377834c1ec381b5b3072981373a4230e40ec
mdfauser <fauser.negotium@gmail.com>
2026-03-19 16:00:17 +0100

    feat: compute_n_step_targets in PER

--- storage/buffer.py
+++ storage/buffer.py
@@ -19,6 +19,8 @@ class PrioritizedReplayBuffer:
     data: MuZeroTransition
     priorities: chex.Array
     position: int
+    max_size: int
+    size: int
 
     def add_trajectory(self, new_traj, slot):
         # Update the nested Transition data
@@ -27,21 +29,37 @@ class PrioritizedReplayBuffer:
             self.data,
             new_traj
         )
-        # Return a new buffer object with the updated data
-        return self.replace(data=new_data)
+
+        new_pos = (self.position + 1) % self.max_size
+        new_size = jnp.minimum(self.size + 1, self.max_size)
+
+        return self.replace(data=new_data, position=new_pos, size=new_size)
 
     def sample(self, key, batch_size):
         probs = self.priorities / jnp.sum(self.priorities)
+
         idx = jax.random.choice(key, self.size, shape=(
             batch_size, ), p=probs[:self.size])
+
         return jax.tree_util_map(lambda x: x[idx], self.data)
 
-    def compute_n_step_targets(self, n_steps, disocunt_factor):
+    def compute_n_step_targets(self, rewards, values, n_steps, gamma):
         """Calculate the N-step returns."""
-        pass
-        # n_step_return = jax.tree_util.tree_map(
-        #     lambda reward, value:
-        # )
+        bootstrap_values = jnp.roll(values, -n_steps)
+
+        discounted_bootstrap = (gamma ** n_steps) * bootstrap_values
+
+        kernel = gamma ** jnp.arange(n_steps)
+
+        padded_rewards = jnp.concatenate([rewards, jnp.zeros(n_steps-1)])
+
+        reward_sums = jax.scipy.signal.convolve(
+            padded_rewards,
+            kernel[::-1],
+            mode='valid'
+        )
+
+        return reward_sums[:len(rewards)] + discounted_bootstrap
 
     def update_priorities(self, indices, new_td_errors):
         pass
@@ -67,5 +85,7 @@ def init_prioritized_buffer(max_seq, seq_len, obs_shape):
     return PrioritizedReplayBuffer(
         data=buffer_data,
         priorities=jnp.zeros((max_seq,)),
-        position=0
+        position=0,
+        max_size=1000,
+        size=0
     )
