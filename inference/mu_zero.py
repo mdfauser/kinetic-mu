2328f568f8a9d73ed5a1f333fc3f595910569cdf
mdfauser <fauser.negotium@gmail.com>
2026-03-28 11:30:31 +0100

    feat: game_to_jax

--- inference/mu_zero.py
+++ inference/mu_zero.py
@@ -6,14 +6,37 @@ from storage.buffer import MuZeroTransition, PrioritizedReplayBuffer
 
 
 class MuZero():
-    def __init__(self, num_simulations):
+    def __init__(self, batch_size, num_simulations, max_seq, seq_len, obs_shape, key, unroll_steps, ):
         self.mcts = MuZeroMCTS(num_simulations=num_simulations)
+        self.max_seq
+        self.buffer = init_prioritized_buffer(max_seq=max_seq, seq_len=seq_len, obs_shape=obs_shape)
+        self.key = key
+        self.batch_size = batch_size
+        self.unroll_steps = unroll_steps
+
+    def game_to_jax(self, game_list, max_len):
+    # 1. Convert list of dicts/tuples into a single PyTree of arrays
+        # This turns [{obs: 1}, {obs: 2}] into {obs: [1, 2]}
+        game_tree = jax.tree_util.tree_map(lambda *xs: jnp.array(xs), *game_list)
+        
+        # 2. Calculate padding
+        current_len = len(game_list)
+        pad_amount = max_len - current_len
+        
+        # 3. Apply padding (zeros) so it fits your buffer's 2D structure
+        def pad_fn(x):
+            # Adds zeros to the 'time' dimension
+            pad_shape = [(0, pad_amount)] + [(0, 0)] * (x.ndim - 1)
+            return jnp.pad(x, pad_shape)
+        
+        return jax.tree_util.tree_map(pad_fn, game_tree), current_len
 
     def train(self, env, real_obs):
 
         # STEP 1 - Acting
         # TODO get root_value, child_visits, action_weights
-        action, = self.mcts.select_action(params=None, rng_key=None,
+        game_traj = []
+        action, root_value, child_visits = self.mcts.select_action(params=None, rng_key=None,
                                           real_observation=real_obs)
         # take step in the environment
         obs, reward, terminated, info = env.step(action)
@@ -21,9 +44,15 @@ class MuZero():
         # action_weights are the normalized visit counts of the root -> policy
         # the search_tree provides a value in the root (weighted average of all rewards and future values discovered in dream steps)
         # self.buffer.store(policy_output.action_weights, self._current_obs, self._current_action, policy_output.search_tree.summary().value)
-        transition = MuZeroTransition(
-            observation=real_obs, action=action, reward=reward, root_value=None, child_visits=None)
-        # TODO add transition into game trajectory, implement for buffer add single steps of the game at a time
+        # transition = MuZeroTransition(
+            # observation=real_obs, action=action, reward=reward, root_value=None, child_visits=None)
+        transition = [real_obs, action, reward, root_value, child_visits]
+        game_traj.append(transition)
+        game_len = len(game_traj)
+        jax_game_traj = self.game_to_jax(game_traj, self.max_seq) # TODO
+        self.buffer.add_game(jax_game_traj, game_len)
         # STEP 2 - Planning
-
         # STEP 3 - Learning
+        # TODO make unroll_steps maybe part of the obj instead of parameters
+        prioritized_samples = self.buffer.prioritized_sample(self.key, self.unroll_steps, self.batch_size)
+        
